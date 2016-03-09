37a78fb3dd8162e365b619fabc1a07a660ee4e83
Ben Green <b.green@warwick.ac.uk>
2016-03-09 08:13:29 +0000

    Bug fixes for tau-type experiments; implementation of two further pulse sequences (Hahn echo; echo decay)
    Tau window will now accept keyboard input, and enables / disables the auto-generated controls as appropriate

--- PulseSequenceDemo.fsx
+++ PulseSequenceDemo.fsx
@@ -20,30 +20,9 @@ Async.Start <| async {
     let uwX = Channel.Channel2
     let uwY = Channel.Channel3
 
-    let pulse = seq { 
-        yield Pulse.create      [acq]               10u
-        yield Pulse.empty                           30u
-        yield Pulse.create      [laser]             20u
-        yield Pulse.create      [acq; laser]        20u
-        yield Pulse.empty                           300u }
-
     let stop = seq {
         yield Pulse.empty                           1u
         }
-
-    let rabiSequence numberOfPoints ShotsPerPoint  PulseStreamerMicrowaveChannel InitialPulseDuration PulseDurationChange PulseStreamerAcquisitionChannel PulseStreamerLaserChannel DetectionLaserDuration = 
-         seq { for i in 0..(numberOfPoints - 1) do     
-                 for j in 0..(ShotsPerPoint - 1) do      
-                     yield Pulse.create 
-                           <| [PulseStreamerMicrowaveChannel] 
-                           <| (uint32 (InitialPulseDuration + i * PulseDurationChange))
-                     yield Pulse.create
-                           <| [PulseStreamerLaserChannel; PulseStreamerAcquisitionChannel]
-                           <| 5u
-                     yield Pulse.create 
-                           <| [PulseStreamerLaserChannel]
-                           <| (uint32 (DetectionLaserDuration)) }
-         |> Pulse.Transform.compensateHardwareDelays [PulseStreamerLaserChannel] (uint32 10)
 
     let turnOnMicrowaves = Seq.singleton (Pulse.create [uwY] 100u)
     let turnOnLaser = Seq.singleton (Pulse.create [laser] 100u)
@@ -63,10 +42,7 @@ Async.Start <| async {
             yield Pulse.empty 2000u
         }
 
-    let rS = rabiSequence 100 1 Channel.Channel2 0 1 Channel.Channel0 Channel.Channel1 4000
-
     let! streamer = PulseStreamer.openDevice("http://192.168.1.100:8050/json-rpc")
-
     let sequence = turnOnLaser //AndMicrowaves
 
     do! PulseStreamer.PulseSequence.writeSequence
@@ -75,12 +51,6 @@ Async.Start <| async {
         <| finalState
         <| errorState
         <| streamer
-    //do! PulseStreamer.PulseSequence.writeSequence pulse 10000000u finalState errorState streamer 
-    //do! PulseStreamer.PulseSequence.writeSequence rS 0u finalState errorState streamer
-    //do! PulseStreamer.PulseSequence.writeSequence turnOnMicrowaves 0u finalState errorState streamer 
-    //do! PulseStreamer.PulseSequence.writeSequence microwaveTest 0u finalState errorState streamer 
-    //do! PulseStreamer.PulseSequence.writeSequence turnOnLaser 0u finalState errorState streamer
-    //do! PulseStreamer.PulseSequence.writeSequence turnOnLaserAndMicrowaves 0u finalState errorState streamer
-    //do! PulseStreamer.PulseSequence.writeSequence stop 10u finalState errorState streamer
-    printfn "Done..."
+
+    printfn "Done."
 }
